5cd5ba6e99f36e9a77e41c6ebf0cbeb61b046ff6
devsungmin <kimsungmin.dev@gmail.com>
2020-04-25 19:43:02 +0900

    멀티 이미지 업로드

--- drag-uploder/src/components/FileUploader.vue
+++ drag-uploder/src/components/FileUploader.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container class="inputdata">
     <h1>File Uploader Page</h1>
-
-    <input type="file" @change="uploadImage" name="image" id="image"  accept="image/*" multiple >
-    <v-btn class="btn btn-outline-info" @click.prevent="submit">서버에 전송</v-btn>
-
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" ref="file" name="image" id="image"  accept="image/*" multiple/>
+      <v-btn class="btn btn-outline-info" @click="submit()">서버에 전송</v-btn>
+    </form>
   </v-container>
 </template>
 
@@ -13,21 +13,25 @@ import axios from "axios";
 export default {
   name: "FileUploader",
   methods: {
-    uploadImage(e) {
-      let img = e.target.files[0];
-      let fd = new FormData();
-      fd.append("image", img);
-      const url = "http://127.0.0.1:8000/"
-      axios.post(url, fd).then(resp => {
-        this.imagePath = resp.data.path;
-        console.log(resp);
-      });
-    },
     submit() {
-      let data = {
-        imagePath: this.imagePath,
-      };
-      console.log(data) 
+      let fd = new FormData();
+      const url = "http://127.0.0.1:8000/users/id/data"
+      for (let i = 0; i < this.$refs.file.files.length; i++ ) {
+        let img = this.$refs.file.files[i];
+        console.log(img)
+        fd.append('image', img);
+        console.log(fd)
+      }
+      axios.post(url, fd, {
+        headers:{
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then(resp => {
+      this.imagePath = resp.data.path;
+      console.log(resp);
+      }).catch(err => {
+          console.log(err);
+      });
     }
   }
 };
